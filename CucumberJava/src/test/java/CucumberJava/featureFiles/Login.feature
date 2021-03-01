fd6cecd5d195de82ad12f399fcbf715885894699
Issa Fabiala <issakarissefabiala@gmail.com>
2021-03-01 14:22:51 +0100

    ++ Creating the last selenium codes to auto test
    Auto test is succes

--- CucumberJava/src/test/java/CucumberJava/featureFiles/Login.feature
+++ CucumberJava/src/test/java/CucumberJava/featureFiles/Login.feature
@@ -1,7 +1,7 @@
 Feature: Login into account
 	Existing user should be able to login to account using correct credentials
 
-Scenario: Login into account correct credentials
+Scenario: Login into 	 correct credentials
 Given User navigates to stackoverflow website
 And User clicks on the login button on homepage 
 And User enters a correct username
--- CucumberJava/src/test/java/CucumberJava/steps/LoginSteps.java
+++ CucumberJava/src/test/java/CucumberJava/steps/LoginSteps.java
@@ -2,8 +2,10 @@ package CucumberJava.steps;
 
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
@@ -34,60 +36,64 @@ public class LoginSteps {
 
 	@Given("^User enters a correct username$")
 	public void user_enters_a_correct_username() throws Throwable {
-		System.out.println("user_enters_a_correct_username");
-
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("issakarissefabiala@gmail.com");
 	}
 
 	@Given("^User enters a correct password$")
 	public void user_enters_a_correct_password() throws Throwable {
-		System.out.println("user_enters_a_correct_password");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("59iWwy,ra/L&JTx");
 	}
+	
 
 	@When("^User clicks on the login button$")
 	public void user_clicks_on_the_login_button() throws Throwable {
-		System.out.println("user_clicks_on_the_login_button");
+		driver.findElement(By.xpath("//button[@id='submit-button']")).click();
 	}
 
 	@Then("^User should be taken to the succesful login page$")
 	public void user_should_be_taken_to_the_succesful_login_page() throws Throwable {
-		System.out.println("user_should_be_taken_to_the_succesful_login_page");
+		Thread.sleep(5000);
+		WebElement askQuestionsButton = driver.findElement(By.xpath("//a[contains(text(),'Ask Question')]"));
+		Assert.assertEquals(true, askQuestionsButton.isDisplayed());
+//		System.out.println("user_should_be_taken_to_the_succesful_login_page2");
 	}
 	
-	
-	
-	
-	@Given("^User navigates to stackoverflow website(\\d+)$")
-	public void user_navigates_to_stackoverflow_website(int arg1) throws Throwable {
-		System.out.println("user_navigates_to_stackoverflow_website2");
-	}
-
-	@Given("^User clicks on the login button on homepage(\\d+)$")
-	public void user_clicks_on_the_login_button_on_homepage(int arg1) throws Throwable {
-		System.out.println("user_clicks_on_the_login_button_on_homepage2");
-	}
-
-	@Given("^User enters a correct username(\\d+)$")
-	public void user_enters_a_correct_username(int arg1) throws Throwable {
-		System.out.println("user_enters_a_correct_username2");
-
-	}
-
-	@Given("^User enters a correct password(\\d+)$")
-	public void user_enters_a_correct_password(int arg1) throws Throwable {
-		System.out.println("user_enters_a_correct_password2");
-
-	}
-
-	@When("^User clicks on the login button(\\d+)$")
-	public void user_clicks_on_the_login_button(int arg1) throws Throwable {
-		System.out.println("user_clicks_on_the_login_button2");
-	}
-
-	@Then("^User should be taken to the succesful login page(\\d+)$")
-	public void user_should_be_taken_to_the_succesful_login_page(int arg1) throws Throwable {
-		System.out.println("user_should_be_taken_to_the_succesful_login_page2");
-	}
-
 }
-
-
+	
+//
+//	
+//	@Given("^User navigates to stackoverflow website(\\d+)$")
+//	public void user_navigates_to_stackoverflow_website(int arg1) throws Throwable {
+//		System.out.println("user_navigates_to_stackoverflow_website2");
+//	}
+//
+//	@Given("^User clicks on the login button on homepage(\\d+)$")
+//	public void user_clicks_on_the_login_button_on_homepage(int arg1) throws Throwable {
+//		System.out.println("user_clicks_on_the_login_button_on_homepage2");
+//	}
+//
+//	@Given("^User enters a correct username(\\d+)$")
+//	public void user_enters_a_correct_username(int arg1) throws Throwable {
+//		System.out.println("user_enters_a_correct_username2");
+//
+//	}
+//
+//	@Given("^User enters a correct password(\\d+)$")
+//	public void user_enters_a_correct_password(int arg1) throws Throwable {
+//		System.out.println("user_enters_a_correct_password2");
+//
+//	}
+//
+//	@When("^User clicks on the login button(\\d+)$")
+//	public void user_clicks_on_the_login_button(int arg1) throws Throwable {
+//		System.out.println("user_clicks_on_the_login_button2");
+//	}
+//
+//	@Then("^User should be taken to the succesful login page(\\d+)$")
+//	public void user_should_be_taken_to_the_succesful_login_page(int arg1) throws Throwable {
+//		System.out.println("user_should_be_taken_to_the_succesful_login_page2");
+//	}
+//
+//}
+//
+//
